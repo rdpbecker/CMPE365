039f9b554edc650f9027b631e5327690df2bef27
Riley <rileydbecker@gmail.com>
2018-10-19 17:15:30 -0400

    Change and compile the graph population functions

--- Project/Code/populateGraph.py
+++ Project/Code/populateGraph.py
@@ -1,3 +1,5 @@
+import printing
+
 def findMax(lists):
     maxVal = lists[0][-1]
     for i in range(1,len(lists)):
@@ -8,12 +10,13 @@ def findMax(lists):
 def toBinary(aList,n):
     list1 = []
     count = 0
+    length = len(aList)
     for i in range(n):
-        if aList[count] == (i+1):
-            list1[i] = 1
+        if (count < length) and (aList[count] == (i+1)):
+            list1.append(1)
             count = count + 1
         else:
-            list1[i] = 0
+            list1.append(0)
     return list1
 
 def addLists(list1,list2):
@@ -26,12 +29,15 @@ def initGraph(lists):
     maxVal = findMax(lists)
     graph = {}
     for i in range(maxVal):
-        graph[i] = [0]*maxVal
+        graph[i+1] = [0]*maxVal
     return graph
 
 def populateGraph(graph,lists):
+    maxVal = findMax(lists)
     for aList in lists:
         list1 = toBinary(aList,maxVal)
+        print aList
         for i in range(maxVal):
             if list1[i] == 1:
-                graph[i] = addLists(list1,graph[i])
+                graph[i+1] = addLists(list1,graph[i+1])
+        printing.printGraph(graph)
