41d02f01bd48d3bd5507fa9e9f8f4339e6f2dbca
Riley <rileydbecker@gmail.com>
2018-10-20 15:41:30 -0400

    Add some command line arguments for more control

--- Project/Code/main.py
+++ Project/Code/main.py
@@ -1,18 +1,16 @@
-def main(threshold):
-    filepath = "../Test Data/testList1.csv"
+def main(threshold,testnum,flag):
+    filepath = "../Test Data/testList"+str(testnum)+".csv"
     lists = io.readFile(filepath)
-    io.printLists(lists)
+    printing.printLists(lists)
     graph = popG.initGraph(lists)
-    printing.printGraph(graph)
     popG.populateGraph(graph,lists)
     print "Final Graph:"
     printing.printGraph(graph)
     connected = find.aboveThreshold(graph,threshold)
     print "The connections are"
     printing.printGraph(connected)
-    cycles = find.findCycles(connected,popG.findMax(lists))
-    printing.printLists(cycles)
+    cycles = find.findCycles(connected,popG.findMax(lists),flag)
 
 if __name__ == "__main__":
     import sys, fileio as io, populateGraph as popG, printing, findConnectedFlyers as find
-    main(int(sys.argv[1]))
+    main(int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[2]))
