280285aa82b2740f2b7080c8a24f010c789ce67e
Riley <rileydbecker@gmail.com>
2018-10-25 14:07:19 -0400

    Comment the printing file

--- Project/Code/printing.py
+++ Project/Code/printing.py
@@ -1,6 +1,22 @@
+##############################################################
+## Prints a list of lists
+##
+## Parameters: lists - the list of lists to be printed
+## 
+## Returns: nothing
+##############################################################
+
 def printLists(lists):
     for aList in lists:
         print aList
+
+##############################################################
+## Prints a graph
+##
+## Parameters: graph - the graph to be printed
+## 
+## Returns: nothing
+##############################################################
 
 def printGraph(graph):
     for key in graph.keys():
